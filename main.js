81fe5bbabe21681cacdda94ade0303a35505ad4f
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-04-13 13:15:17 +0200

    Pausa en el video
    14:39

---
+++ main.js
@@ -0,0 +1,6 @@
+//alert('Hello world');
+//console.log('Hello World Console');
+//console.error('This is an error!');
+//console.warn('This is an warning!');
+let age = 24;
+console.log("Local variable age = "+age);
